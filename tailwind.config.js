37aab537bbe7d721e94fc9e18248ac7ca97f9dc4
mfikri-0298 <thiefkidx@gmail.com>
2021-11-01 20:53:20 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [
+    './page/**/*.js',
+    './page/**/*.jsx',
+    './page/**/*.ts',
+    './page/**/*.tsx',
+    './components/**/*.js',
+    './components/**/*.jsx',
+    './components/**/*.ts',
+    './components/**/*.tsx',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
